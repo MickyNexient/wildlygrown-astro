701ed2fc46abf9df1db276c860c95f7e9d477ce4
micky.hallabrin <micky.hallabrin@nexient.com>
2022-08-20 19:41:23 -0400

    added integration with subabase

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly SUPABASE_KEY: string;
+    readonly SUPABASE_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
